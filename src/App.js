18b40dbfcced4db1534943dbd859ff363e610d4a
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2022-07-29 23:14:21 +0700

    loop-state, change state

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ function App() {
 
   // state
   let [name, setName] = useState("huy")
+  const [todos, setTodo] = useState([
+    { id: 'todo1', title: 'watching todo' },
+    { id: 'todo2', title: 'watching doto' },
+    { id: 'todo3', title: 'watching uu' }
+  ])
 
   const handleEvenClick = (event) => {
-
-    console.log("hello ", name)
+    if (!name) {
+      alert('errol')
+      return
+    }
+    let dotolist = { id: Math.floor(Math.random() * 100), title: name };
+    setTodo([...todos, dotolist])
+    setName('')
   }
   const handleEventOnchange = (event) => {
     setName(event.target.value)
   }
+  //
+
+
 
 
   return (
@@ -24,7 +37,15 @@ function App() {
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello word react with {name}</h1>
+        <h1>hello word react with ziille</h1>
+        <div className='Todo-container'>
+          {todos.map(todo => {
+            return (
+              <div className='Todo-child' id={todo.id}>{todo.title}</div>
+            )
+          })}
+
+        </div>
         <input type="text" value={name} onChange={handleEventOnchange}></input>
         <button onClick={handleEvenClick}>click me</button>
 
