3938d7b02d5d4de094c0e85cb5b9bfe6e1a033c6
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2022-07-29 18:02:29 +0700

    event

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Nav from './views/Nav';
 
 function App() {
   let name = 'hook by huy'
+
+  const handleEvenClick = (event) => {
+    console.log("hello", event.target.value)
+  }
+
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
-        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>hello word react {name}</h1>
+        <input type="text" value="Huy" onChange={handleEvenClick}></input>
+        <button onClick={handleEvenClick}>click me</button>
 
       </header>
     </div>
