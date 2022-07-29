a3ca85637438b4476cf71c53848e0160143b50c8
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2022-07-29 17:11:27 +0700

    up css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
 
 function App() {
+  let name = 'hook by huy'
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello word react hook</h1>
+        <h1>hello word react {name}</h1>
 
       </header>
     </div>
