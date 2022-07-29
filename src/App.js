b8628b1eec54e9a8d6f8109711a518fc99445b59
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2022-07-29 22:39:19 +0700

    use state - setName

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react'
+
+
 
 function App() {
-  let name = 'hook by huy'
+
+  // state
+  let [name, setName] = useState("huy")
 
   const handleEvenClick = (event) => {
-    console.log("hello", event.target.value)
+
+    console.log("hello ", name)
+  }
+  const handleEventOnchange = (event) => {
+    setName(event.target.value)
   }
 
 
@@ -15,8 +24,8 @@ function App() {
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello word react {name}</h1>
-        <input type="text" value="Huy" onChange={handleEvenClick}></input>
+        <h1>hello word react with {name}</h1>
+        <input type="text" value={name} onChange={handleEventOnchange}></input>
         <button onClick={handleEvenClick}>click me</button>
 
       </header>
